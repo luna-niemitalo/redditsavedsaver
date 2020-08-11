caa1cd1ab8cfe4da160cb28ae7d550f8f072727b
Jani Niemitalo <jani.niemitalo@processgenius.fi>
2020-08-11 11:18:07 +0300

    fix index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $files = include("./api/getData.php")
 <script type="text/javascript">
     const dataObj = <?php echo($files)?>;
     const data = []
-    for (const value of dataObj) {
+    for (const value of Object.values(dataObj)) {
         data.push(value);
     }
     function imgclick(id) {
